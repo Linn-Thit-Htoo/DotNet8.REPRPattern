996f04e54a336f88fa936016936cc4f15e52a25a
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-12 01:08:01 +0630

    -

--- DotNet8.REPRPattern.Api/Dependencies/DependencyInjection.cs
+++ DotNet8.REPRPattern.Api/Dependencies/DependencyInjection.cs
@@ -2,36 +2,35 @@
 using DotNet8.REPRPattern.Api.Extensions;
 using DotNet8.REPRPattern.Api.Features.Blog;
 
-namespace DotNet8.REPRPattern.Api.Dependencies
+namespace DotNet8.REPRPattern.Api.Dependencies;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder).AddMediatRService().AddValidatorService();
-        }
+        return services.AddDbContextService(builder).AddMediatRService().AddValidatorService();
+    }
 
-        private static IServiceCollection AddDbContextService(this  IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(this  IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddMediatRService(this IServiceCollection services)
-        {
-            return services.AddMediatR(cf =>
-                cf.RegisterServicesFromAssembly(typeof(Extension).Assembly)
-            );
-        }
+    private static IServiceCollection AddMediatRService(this IServiceCollection services)
+    {
+        return services.AddMediatR(cf =>
+            cf.RegisterServicesFromAssembly(typeof(Extension).Assembly)
+        );
+    }
 
-        private static IServiceCollection AddValidatorService(this IServiceCollection services)
-        {
-            return services.AddScoped<BlogValidator>();
-        }
+    private static IServiceCollection AddValidatorService(this IServiceCollection services)
+    {
+        return services.AddScoped<BlogValidator>();
     }
 }

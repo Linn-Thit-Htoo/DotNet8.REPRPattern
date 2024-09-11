8a4d8d020c5ee28465088381038c8b865be09a06
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-11 22:32:47 +0630

    [ Added ] DI

--- DotNet8.REPRPattern.Api/Dependencies/DependencyInjection.cs
+++ DotNet8.REPRPattern.Api/Dependencies/DependencyInjection.cs
@@ -1,6 +1,32 @@
-﻿namespace DotNet8.REPRPattern.Api.Dependencies
+﻿using DotNet8.REPRPattern.Api.Db;
+using DotNet8.REPRPattern.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.REPRPattern.Api.Dependencies
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            return services.AddDbContextService(builder).AddMediatRService();
+        }
+
+        private static IServiceCollection AddDbContextService(this  IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+            return services;
+        }
+
+        private static IServiceCollection AddMediatRService(this IServiceCollection services)
+        {
+            return services.AddMediatR(cf =>
+                cf.RegisterServicesFromAssembly(typeof(Extension).Assembly)
+            );
+        }
     }
 }

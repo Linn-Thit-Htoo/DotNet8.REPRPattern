cdd0a4f3bbf618db60ecdd83169e75120a757fc7
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-12 01:08:58 +0630

    -

--- DotNet8.REPRPattern.Api/Entities/Result.cs
+++ DotNet8.REPRPattern.Api/Entities/Result.cs
@@ -9,22 +9,46 @@ public class Result<T>
 
     public static Result<T> Success(string message = "Success.")
     {
-        return new Result<T> { Message = message, IsSuccess = true, StatusCode = EnumStatusCode.Success };
+        return new Result<T>
+        {
+            Message = message,
+            IsSuccess = true,
+            StatusCode = EnumStatusCode.Success
+        };
     }
 
     public static Result<T> Success(T data, string message = "Success.")
     {
-        return new Result<T> { Data = data, Message = message, IsSuccess = true, StatusCode = EnumStatusCode.Success };
+        return new Result<T>
+        {
+            Data = data,
+            Message = message,
+            IsSuccess = true,
+            StatusCode = EnumStatusCode.Success
+        };
     }
 
-    public static Result<T> Fail(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+    public static Result<T> Fail(
+        string message = "Fail.",
+        EnumStatusCode statusCode = EnumStatusCode.BadRequest
+    )
     {
-        return new Result<T> { Message = message, IsSuccess = false, StatusCode = statusCode };
+        return new Result<T>
+        {
+            Message = message,
+            IsSuccess = false,
+            StatusCode = statusCode
+        };
     }
 
     public static Result<T> Fail(Exception ex)
     {
-        return new Result<T> { Message = ex.ToString(), IsSuccess = false, StatusCode = EnumStatusCode.InternalServerError };
+        return new Result<T>
+        {
+            Message = ex.ToString(),
+            IsSuccess = false,
+            StatusCode = EnumStatusCode.InternalServerError
+        };
     }
 
     public static Result<T> Duplicate(string message = "Duplicate data.")

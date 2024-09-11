733cb3371cbe65487b3faff48540cdc8eaeb5296
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-12 01:00:01 +0630

    [ Added ] DeleteBlog Endpoint

--- DotNet8.REPRPattern.Api/Features/Blog/DeleteBlog/DeleteBlogEndpoint.cs
+++ DotNet8.REPRPattern.Api/Features/Blog/DeleteBlog/DeleteBlogEndpoint.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.REPRPattern.Api.Features.Blog.DeleteBlog
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DeleteBlogEndpoint : ControllerBase
+    public class DeleteBlogEndpoint : BaseController
     {
+        private readonly IMediator _mediator;
+
+        public DeleteBlogEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBlog(int id, CancellationToken cancellationToken)
+        {
+            var command = new DeleteBlogCommand(id);
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Content(result);
+        }
     }
 }

414b58dbb51c9fd4d695a224e233fa49af8a68bd
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-12 01:09:22 +0630

    -

--- DotNet8.REPRPattern.Api/Entities/Tbl_Blog.cs
+++ DotNet8.REPRPattern.Api/Entities/Tbl_Blog.cs
@@ -1,15 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace DotNet8.REPRPattern.Api.Entities;
 
-namespace DotNet8.REPRPattern.Api.Entities
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
--- DotNet8.REPRPattern.Api/GlobalUsings.cs
+++ DotNet8.REPRPattern.Api/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using DotNet8.REPRPattern.Api.Db;
 global using DotNet8.REPRPattern.Api.Extensions;
 global using DotNet8.REPRPattern.Api.Features.Blog;
 global using DotNet8.REPRPattern.Api.Enums;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;

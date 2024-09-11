737bdb288a652f947bd154802cd7e674e108a366
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-12 00:52:32 +0630

    [ Added ] UpdateBlog

--- DotNet8.REPRPattern.Api/Features/Blog/UpdateBlog/UpdateBlogEndpoint.cs
+++ DotNet8.REPRPattern.Api/Features/Blog/UpdateBlog/UpdateBlogEndpoint.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.REPRPattern.Api.Features.Blog.UpdateBlog
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UpdateBlogEndpoint : ControllerBase
+    public class UpdateBlogEndpoint : BaseController
     {
+        private readonly IMediator _mediator;
+
+        public UpdateBlogEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateBlog([FromBody] UpdateBlogRequestDTO updateBlogRequest, int id, CancellationToken cancellationToken)
+        {
+            var command = new UpdateBlogCommand(updateBlogRequest, id);
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Content(result);
+        }
     }
 }

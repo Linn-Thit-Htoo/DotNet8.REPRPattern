542a3c6a5567037125d51f7cce87014ef583b68d
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-11 22:38:22 +0630

    [ Added ] MessageResource

---
+++ DotNet8.REPRPattern.Api/Entities/Result.cs
@@ -0,0 +1,6 @@
+﻿namespace DotNet8.REPRPattern.Api.Entities
+{
+    public class Result
+    {
+    }
+}
---
+++ DotNet8.REPRPattern.Api/Features/PageSetting/PageSettingDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNet8.REPRPattern.Api.Features.PageSetting
+{
+    public class PageSettingDTO
+    {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public int TotalCount { get; set; }
+
+        public PageSettingDTO(int pageNo, int pageSize, int pageCount, int totalCount)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+            PageCount = pageCount;
+            TotalCount = totalCount;
+        }
+    }
+}

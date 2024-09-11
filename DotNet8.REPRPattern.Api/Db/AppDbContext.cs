79448729b0510de3426d4f4c9629ffc00675e8f0
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-11 22:23:09 +0630

    [ Added ] AppDbContext

--- DotNet8.REPRPattern.Api/Db/AppDbContext.cs
+++ DotNet8.REPRPattern.Api/Db/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace DotNet8.REPRPattern.Api.Db
+﻿using DotNet8.REPRPattern.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.REPRPattern.Api.Db
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
     }
 }
--- DotNet8.REPRPattern.Api/Features/Blog/GetBlogList/GetBlogListDTO.cs
+++ DotNet8.REPRPattern.Api/Features/Blog/GetBlogList/GetBlogListDTO.cs
@@ -1,6 +1,9 @@
-﻿namespace DotNet8.REPRPattern.Api.Features.Blog.GetBlogList
+﻿using DotNet8.REPRPattern.Api.Entities;
+
+namespace DotNet8.REPRPattern.Api.Features.Blog.GetBlogList
 {
     public class GetBlogListDTO
     {
+        public IQueryable<Tbl_Blog> Blogs { get; set; }
     }
 }
